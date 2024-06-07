906095879e0f3aaae23b8d9949e44e50dd43f6d3
Yaphet Kebede <kebedey@ad.renci.org>
2024-06-07 15:59:21 -0400

    adding langserve

---
+++ src/chain.py
@@ -0,0 +1,90 @@
+import config
+from langchain_community.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
+from typing import Any
+from langchain_community.docstore.document import Document
+from langchain_community.llms import Ollama
+from langserve import CustomUserType
+import json
+import fastapi
+from langserve import add_routes
+
+app = fastapi.FastAPI()
+
+class Question(CustomUserType):
+    question: str
+
+
+
+
+
+class CustomQdrant(Qdrant):
+    @classmethod
+    def get_study_data(cls, study_id):
+        with open(config.STUDIES_JSON_FILE) as stream:
+            data = json.load(stream)
+        for study in data:
+            if study['StudyName'] == study_id:
+                return study['StudyName'] + '(' + study['Permalink'] + '): ' + '\n' + study['Description']
+        return ""
+
+    @classmethod
+    def _document_from_scored_point(
+            cls,
+            scored_point: Any,
+            collection_name: str,
+            content_payload_key: str,
+            metadata_payload_key: str,
+    ) -> Document:
+        '''
+        This method is overriden to get the documents form a local file to provide for the context.
+        :param scored_point:
+        :param collection_name:
+        :param content_payload_key:
+        :param metadata_payload_key:
+        :return:
+        '''
+        metadata = scored_point.payload.get(metadata_payload_key) or {}
+        metadata["_id"] = scored_point.id
+        metadata["_collection_name"] = collection_name
+        study_id = scored_point.payload.get('question_id').split('.')[0]
+        metadata["study_id"] = study_id
+        metadata["score"] = scored_point.score
+        page_content = CustomQdrant.get_study_data(study_id)
+        return Document(
+            page_content=page_content,
+            metadata=metadata,
+        )
+
+def init_chain():
+    client = config.AQClient
+    embeddings = config.ollama_emb
+    doc_store = CustomQdrant(
+        async_client=config.AQClient,
+        collection_name=config.QDRANT_COLLECTION_NAME,
+        embeddings=embeddings,
+        client=config.QClient
+    )
+    ollama = Ollama(
+        base_url=config.OLLAMA_URL,
+        model=config.GEN_MODEL_NAME
+    )
+    qachain = RetrievalQA.from_chain_type(ollama, retriever=doc_store.as_retriever(
+        search_type="similarity_score_threshold",
+        # I don't think this is working
+        search_kwargs={'score_threshold': 0.8, 'k': 3}
+    ), ).with_types(input_type=Question)
+
+    return qachain
+
+add_routes(
+    app,
+    init_chain(),
+    path='/dug-qa',
+    input_type=Question
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
--- src/config.py
+++ src/config.py
@@ -1,13 +1,13 @@
 from langchain_community.embeddings import OllamaEmbeddings
-from qdrant_client import QdrantClient
+from qdrant_client import QdrantClient, async_qdrant_client
 import os
 from pathlib import Path
 OLLAMA_URL = os.getenv('OLLAMA_URL', 'http://localhost:11434').rstrip('/')
-QDRANT_URL = os.getenv('QDRANT_URL', 'http://localhost:53814').rstrip('/')
+QDRANT_URL = os.getenv('QDRANT_URL', 'http://localhost:6333').rstrip('/')
 GEN_MODEL_NAME = os.getenv("GEN_MODEL_NAME", "llama3:latest")
 EMB_MODEL_NAME = os.getenv("EMB_MODEL_NAME", "llama3")
-QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
-STUDIES_JSON_FILE = os.getenv("STUDIES_JSON_FILE")
+QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "test_collection2")
+STUDIES_JSON_FILE = os.getenv("STUDIES_JSON_FILE", Path(os.path.dirname(__file__), '..',  'data', '99_select_studies.json'))
 LOG_FILE_PATH = os.getenv("LOG_FILE_PATH", Path(os.path.dirname(__file__), '..' ,  'koios.log'))
 
 ollama_emb = OllamaEmbeddings(model=EMB_MODEL_NAME, base_url=OLLAMA_URL)
@@ -15,3 +15,4 @@ ollama_emb = OllamaEmbeddings(model=EMB_MODEL_NAME, base_url=OLLAMA_URL)
 llm_url = f"{OLLAMA_URL}/api/generate"
 
 QClient = QdrantClient(url=QDRANT_URL)
+AQClient = async_qdrant_client.AsyncQdrantClient(url=QDRANT_URL)
